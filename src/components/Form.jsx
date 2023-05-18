46e0eac001f0727c29a496356830032110fa62cc
MaxiBelli <maximiliano_belli@hotmail.com>
2023-05-18 15:45:50 -0300

    feat: Create hook to access Provider data

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,9 @@
 import { Fragment } from "react";
 import { BRANDS, YEARS, PLANS } from "../constants";
+import useQuoter from "../hooks/useQuoter";
 
 const Form = () => {
+
   return (
     <>
       <form>
